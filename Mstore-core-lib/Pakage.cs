9628a129069dee28d13276bfc4de6467c06ee6ce
Bananaboy319 <bananaboy319@gmail.com>
2021-02-23 13:51:35 +0100

    Download error

--- Mstore-core-lib/Pakage.cs
+++ Mstore-core-lib/Pakage.cs
@@ -15,13 +15,13 @@ namespace Pakages
         public string Description;
         public string Folder;
         public string EXEPath;
-        private string Path;
+        private string Path = "./Mstore/";
 
         public void Download()
         {
-            DownloadManager.Download(URL, path);
-            //TODO: catch errors
-            ExtractToDirectory(Path, Path + Folder);
+            DownloadManager.Download(URL, path + Name);
+            //TODO: catch errors + extract the right zip name
+            ExtractToDirectory(Path + name, Path + Folder);
             Logger.Write("Extract Complete\n " + Name + "\nLocation:  " + Path + Folder);
 
         }
@@ -33,7 +33,8 @@ namespace Pakages
                 Process Launcher = new Process();
                 Launcher.StartInfo.FileName = EXEPath;
                 Launcher.Start();
-            } catch (Win32Exception ex)
+            } 
+            catch (Win32Exception ex)
             {
                 Logger.Write(Name + " Is NOT Downloaded");
                 Directory.Delete(Folder, true)
