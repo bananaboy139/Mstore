a79fbc2db9773a50d133442dc7d5b871eefb0a5a
bananaboy319 <bananaboy319@gmail.com>
2021-02-28 08:43:11 +0100

    fixed Logger

--- Mstore-core-lib/Logger.cs
+++ Mstore-core-lib/Logger.cs
@@ -7,8 +7,10 @@ namespace Mstore_Log_lib
     public class Logger
     {
         static string LogFile = Var.LogFile;
-        public static void Write(string text)
+        public static void Write(string t)
         {
+            DateTime dateToDisplay = new DateTime();
+            string text = dateToDisplay.ToString() + ":   " + t + "\n";
             File.AppendAllText(LogFile, text);
         }
     }
