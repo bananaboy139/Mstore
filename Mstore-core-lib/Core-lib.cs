9866083302c4e7a8147dbccb37951b76cc92afcb
bananaboy139 <matteob319@gmail.com>
2021-04-24 22:10:46 +0200

    added base encryption algorithum

--- Mstore-core-lib/Core-lib.cs
+++ Mstore-core-lib/Core-lib.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
 
 namespace Mstore_Core_lib
 {
@@ -89,31 +91,34 @@ namespace Mstore_Core_lib
             //read pakage files
             if (Config.StoreSecured)
             {
-                foreach (string f in Directory.GetFiles(PakagesFolder, "*.json", SearchOption.TopDirectoryOnly))
+                List<string> ToDecrypt = new List<string>();
+                foreach (string f in Directory.GetFiles(PakagesFolder, "*.aes", SearchOption.TopDirectoryOnly))
                 {
-                    //FIXME: new encryption not working
+                    ToDecrypt.Add(f);
+                }
+
+                foreach (string f in ToDecrypt)
+                {
                     try
                     {
-                        File.Decrypt(f);
+                        Decrypt(f);
+                        File.Delete(f);
                     }
-                    catch (System.IO.IOException e)
+                    catch (IOException e)
                     {
                         Write(e.ToString());
                     }
-                    using StreamReader file = File.OpenText(f);
-                    Pakages.Add((Pakage)serializer.Deserialize(file, typeof(Pakage)));
-                    file.Close();
-
-                    File.Encrypt(f);
                 }
             }
-            else
+            foreach (string f in Directory.GetFiles(PakagesFolder, "*.json", SearchOption.TopDirectoryOnly))
             {
-                foreach (string f in Directory.GetFiles(PakagesFolder, "*.json", SearchOption.TopDirectoryOnly))
+                using StreamReader file = File.OpenText(f);
+                Pakages.Add((Pakage)serializer.Deserialize(file, typeof(Pakage)));
+                file.Close();
+                if (Config.StoreSecured)
                 {
-                    using StreamReader file = File.OpenText(f);
-                    Pakages.Add((Pakage)serializer.Deserialize(file, typeof(Pakage)));
-                    file.Close();
+                    Encrypt(f);
+                    File.Delete(f);
                 }
             }
 
@@ -135,27 +140,19 @@ namespace Mstore_Core_lib
         {
             try
             {
-                if (Config.StoreSecured)
+                foreach (Pakage pakage in Pakages)
                 {
-                    foreach (Pakage pakage in Pakages)
-                    {
-                        string pakageinfo = JsonConvert.SerializeObject(pakage);
-                        string FileName = MstorePath + "Pakages/" + pakage.JName + ".json";
+                    string pakageinfo = JsonConvert.SerializeObject(pakage);
+                    string FileName = PakagesFolder + pakage.JName + ".json";
 
-                        File.WriteAllText(FileName, pakageinfo);
-                        File.Encrypt(FileName);
+                    File.WriteAllText(FileName, pakageinfo);
+                    if (Config.StoreSecured)
+                    {
+                        Encrypt(FileName);
+                        File.Delete(FileName);
                     }
                 }
-                else
-                {
-                    foreach (Pakage pakage in Pakages)
-                    {
-                        string pakageinfo = JsonConvert.SerializeObject(pakage);
-                        string FileName = MstorePath + "Pakages/" + pakage.JName + ".json";
 
-                        File.WriteAllText(FileName, pakageinfo);
-                    }
-                }
 
 
                 //export config
@@ -189,6 +186,156 @@ namespace Mstore_Core_lib
                 Write(d);
                 Directory.Delete(d, true);
             }
+        }
+
+
+        //encryption https://ourcodeworld.com/articles/read/471/how-to-encrypt-and-decrypt-files-using-the-aes-encryption-algorithm-in-c-sharp
+
+        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+        public static extern bool ZeroMemory(IntPtr Destination, int Length);
+
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fille the buffer with the generated data
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        private static void FileEncrypt(string inputFile, string password )
+        {
+            //http://stackoverflow.com/questions/27645527/aes-encryption-on-large-files
+
+            //generate random salt
+            byte[] salt = GenerateRandomSalt();
+
+            //create output file name
+            FileStream fsCrypt = new FileStream(PakagesFolder + Path.GetFileNameWithoutExtension(inputFile) + ".aes", FileMode.Create);
+
+            //convert password string to byte arrray
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            //http://stackoverflow.com/questions/2659214/why-do-i-need-to-use-the-rfc2898derivebytes-class-in-net-instead-of-directly
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            //Cipher modes: http://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+            AES.Mode = CipherMode.CFB;
+
+            // write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    //UI show some stuff
+                    cs.Write(buffer, 0, read);
+                }
+
+                // Close up
+                fsIn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                cs.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        private static void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CFB;
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    //UI show some stuff
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        private static void Encrypt(string file)
+        {
+            FileEncrypt(PakagesFolder + Path.GetFileNameWithoutExtension(file) + ".json", Config.MasterPass);
+        }
+
+        private static void Decrypt(string file)
+        {
+            FileDecrypt(PakagesFolder + Path.GetFileNameWithoutExtension(file) + ".aes",
+                PakagesFolder + Path.GetFileNameWithoutExtension(file) + ".json", Config.MasterPass);
         }
     }
 
@@ -257,11 +404,6 @@ namespace Mstore_Core_lib
                 }
             }
         }
-
-        public void Hash()
-        {
-
-        }
     }
 
     public class Config
@@ -272,5 +414,7 @@ namespace Mstore_Core_lib
         public static bool StorePass = false;
         [JsonProperty]
         public static bool StoreSecured = false;
+
+        public static string MasterPass = "master";
     }
 }
